a055ddc163c6aefe08905c17b4b8881bc61bb135
Juan Diego <jdiegopm12@gmail.com>
2024-01-31 18:39:59 -0500

    Convert code files to C++

--- execute.sh
+++ execute.sh
@@ -5,8 +5,8 @@ compile_extra_args=$3
 exercise_folder='src/exercise_'$exercise_index
 
 if [ "$compile_options" == "compile" ]; then
-  code_file=$(ls -a $exercise_folder | grep -i '.c')
-  gcc $compile_extra_args $exercise_folder'/'$code_file -o $exercise_folder'/.exe'
+  code_file=$(ls -a $exercise_folder | grep -i '.cc')
+  gpp $compile_extra_args $exercise_folder'/'$code_file -o $exercise_folder'/.exe'
 fi
 
 time $exercise_folder'/.exe'
